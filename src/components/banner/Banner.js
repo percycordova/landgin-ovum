df4092069bcdf4f5e321526c291db3c14d330420
percy cordova <cordova_inf@hotmail.com>
2022-03-03 19:02:20 -0500

    arreglando el banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -4,7 +4,8 @@ import Idiomas from '../idiomas/Idiomas'
 const Banner = ({ idiomas }) => {
   return (
 
-    <div className="  relative z-10 py-2 text-white" >
+    <div className="  relative z-10 py-2 text-white  border" >
+
       {/* <div className=" banner relative z-10 py-2 text-white"> */}
       {/* version mobil */}
       {/*  <img
@@ -30,34 +31,13 @@ const Banner = ({ idiomas }) => {
           <img src="/Logo-organizado2 1.svg " alt="" className="sm:hidden z-30 w-80" />
         </div>
       </div>
-      <div className="relative">
+      {/* <div className="relative">
         <img src="/fondo-mobile-nb-top.png" className='absolute  sm:hidden w-full  -mt-48 z-20' alt="" />
         <img src="/fondo-nb-top.png" className='absolute hidden sm:block w-full lg:top-0  -mt-48 lg:-mt-100 2xl:-mt-110  z-20' alt="" />
 
         <img src="/fondo-medio.png" className='absolute  lg:top-0  w-full -mt-52 h-120 sm:-mt-52 sm:h-128 lg:-mt-90 z-0 lg:h-screen' alt="" />
-      </div>
-      <div className="relative h-80">
-        <p className="font-bold text-2.5xl md:text-5xl  z-20 relative mt-30 text-center banner__fecha">
-          {idiomas.Banner.value}
-        </p>
-        <p className="text-center z-20 text-base md:text-4xl relative  banner__san-pedro">
-          San Pedro Sula, Honduras
-        </p>
-        <div className="flex justify-center relative z-20 mt-6 lg:mt-30 w-64 mx-auto ">
-          <a
-            className={`bg-secondary-600 text-white pt-2 pb-3 flex justify-center
-                       items-center w-full text-lg md:text-xl  rounded-3xl 
-                       hover:opacity-80 transition-all duration-300 focus:outline-none 
-                       focus:shadow-outline cursor-pointer mb-4`}
-            href="/pdfs/PATROCINIOS-OVUM-2022.pdf"
-            target="_blank"
-            rel="noopener"
-          >
-            {idiomas.Button.value1}
-          </a>
-        </div>
-        <img src="/fondo-blanco-bottom.png" className='absolute bottom-0  w-full    z-0 ' alt="" />
-      </div>
+      </div> */}
+
       {/* <div className="absolute z-20 bottom-0 banner__gradiente w-full h-4"></div> */}
       {/* version mobil */}
       {/*  <img
@@ -71,7 +51,39 @@ const Banner = ({ idiomas }) => {
         alt=""
         className="hidden md:block absolute -bottom-44 left-0 opacity-20 "
       /> */}
-    </div>
+      <div className=" pp relative w-full  lg:-mt-64 ">
+        <img src="/fondo-mobile-nb-top.png" className='-mt-20 absolute pp2  lg:hidden' alt="" />
+        <img src="/fondo-nb-top.png" className='absolute hidden lg:block fondo-nb-top-desktop -mt-26' alt="" />
+        <img src="/lineas-mobile.svg" alt="" className='absolute -top-10 lineas w-full lg:hidden' />
+        <img src="/lineas-desktop.svg" alt="" className='absolute top-10 right-0 linea hidden lg:block' />
+        <div className='absolute bottom-0 h-20 lg:hidden bg-white w-full'></div>
+        <div className="pt-36 pb-30 lg:pb-44 xl:pb-52 relative z-50  lg:pt-72">
+          <p className="font-bold text-2.5xl md:text-5xl  z-20 mt-4 text-center banner__fecha">
+            {idiomas.Banner.value}
+          </p>
+          <p className="text-center z-20 text-base md:text-4xl relative  banner__san-pedro">
+            San Pedro Sula, Honduras
+          </p>
+          <div className="flex justify-center  z-30 mt-6 lg:absolute lg:bottom-0 btn-banner relative  w-64 mx-auto  ">
+            <a
+              className={`bg-secondary-600 text-white pt-2 pb-3 flex justify-center
+                       items-center w-full text-lg md:text-xl  rounded-3xl 
+                       hover:opacity-80 transition-all duration-300 focus:outline-none 
+                       focus:shadow-outline cursor-pointer mb-4`}
+              href="/pdfs/PATROCINIOS-OVUM-2022.pdf"
+              target="_blank"
+              rel="noopener"
+            >
+              {idiomas.Button.value1}
+            </a>
+          </div>
+          <img src="/fondo-blanco-bottom.png" className='absolute bottom-0  w-full    z-0 ' alt="" />
+        </div>
+        {/* <div className='relative'>
+        <img src="/fondo-nb-top.png" alt="" className='top-30 z-50 absolute w-full ' />
+      </div> */}
+      </div>
+    </div >
   )
 }
 
--- src/sections/landing/conferencia.js
+++ src/sections/landing/conferencia.js
@@ -19,7 +19,7 @@ const Conferencia = () => {
       <div className="absolute top-0 right-0">
         <img src="/LineaConferenciaDer.svg" className='hidden md:block md:w-30 lg:w-36' alt="" />
       </div>
-      <div className="w-full md:w-9/12 mt-5 max-w-7xl mx-auto p-8 lg:p-0">
+      <div className="w-full md:w-9/12 mt-5 max-w-7xl mx-auto p-0 lg:p-0">
         <h6 className="text-3xl lg:text-4xl text-center font-medium mb-8 text-green-300">
           CONFERENCIAS
         </h6>
@@ -48,29 +48,17 @@ const Conferencia = () => {
               },
               1024: {
                 slidesPerView: 3,
-                spaceBetween: 30
+                spaceBetween: 24
+
               }
             }}
             modules={[Navigation, Pagination]}
             className="mySwiper"
           >
-            <SwiperSlide>
-              <div className="w-full p-2 h-80">
-                <div className=" bg-white  h-full px-8 py-5 rounded-b-3xl rounded-tl-3xl shadow-md ">
-                  <div className="">
-                    <h4 className='text-green-300 '>Dia 1</h4>
-                    <h2 className="text-xl pt-3 pb-6">El consumidor del futuro</h2>
-                    <p className='text-base text-justify pb-5'>
-                      “Lider global del comportamiento del consumidor y comprador  global del comportamiento del consumidor y comprador”
-                    </p>
-                  </div>
-                </div>
-              </div>
 
-            </SwiperSlide>
             <SwiperSlide>
-              <div className="w-full p-2 h-80">
-                <div className=" bg-white  h-full px-8 py-5 rounded-b-3xl rounded-tl-3xl shadow-md ">
+              <div className="w-full p-2 h-80 px-10 lg:px-0">
+                <div className=" bg-white  h-full px-6 lg:px-8 py-5 rounded-b-3xl rounded-tl-3xl shadow-md ">
                   <div className="">
                     <h4 className='text-green-300 '>Dia 1</h4>
                     <h2 className="text-xl pt-3 pb-6">El consumidor del futuro</h2>
@@ -82,8 +70,8 @@ const Conferencia = () => {
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="w-full p-2 h-80">
-                <div className=" bg-white  h-full px-8 py-5 rounded-b-3xl rounded-tl-3xl shadow-md ">
+              <div className="w-full p-2 h-80 px-10 lg:px-0">
+                <div className=" bg-white  h-full px-6 lg:px-8 py-5 rounded-b-3xl rounded-tl-3xl shadow-md ">
                   <div className="">
                     <h4 className='text-green-300 '>Dia 1</h4>
                     <h2 className="text-xl pt-3 pb-6">El consumidor del futuro</h2>
@@ -95,8 +83,8 @@ const Conferencia = () => {
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="w-full p-2 h-80">
-                <div className=" bg-white  h-full px-8 py-5 rounded-b-3xl rounded-tl-3xl shadow-md ">
+              <div className="w-full p-2 h-80 px-10 lg:px-0">
+                <div className=" bg-white  h-full px-6 lg:px-8 py-5 rounded-b-3xl rounded-tl-3xl shadow-md ">
                   <div className="">
                     <h4 className='text-green-300 '>Dia 1</h4>
                     <h2 className="text-xl pt-3 pb-6">El consumidor del futuro</h2>
@@ -108,11 +96,24 @@ const Conferencia = () => {
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="w-full p-2 h-80">
-                <div className=" bg-white  h-full px-8 py-5 rounded-b-3xl rounded-tl-3xl shadow-md ">
+              <div className="w-full p-2 h-80 px-10 lg:px-0">
+                <div className=" bg-white  h-full px-6 lg:px-8 py-5 rounded-b-3xl rounded-tl-3xl shadow-md ">
                   <div className="">
-                    <h4 className='text-green-300 '>Dia 1</h4>
-                    <h2 className="text-xl pt-3 pb-6">El consumidor del futuro</h2>
+                    <h4 className='text-green-300 '>Dia 2</h4>
+                    <h2 className="text-xl pt-3 pb-6">Tecnología para la industria Avícola</h2>
+                    <p className='text-base text-justify pb-5'>
+                      “Lider global del comportamiento del consumidor y comprador  global del comportamiento del consumidor y comprador”
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-full p-2 h-80 px-10 lg:px-0 ">
+                <div className=" bg-white  h-full px-6 lg:px-8 py-5 rounded-b-3xl rounded-tl-3xl shadow-md ">
+                  <div className="">
+                    <h4 className='text-green-300 '>Dia 3</h4>
+                    <h2 className="text-xl pt-3 pb-6">La industria como aliada de la seguridad alimentaria</h2>
                     <p className='text-base text-justify pb-5'>
                       “Lider global del comportamiento del consumidor y comprador  global del comportamiento del consumidor y comprador”
                     </p>
