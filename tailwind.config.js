5e47d71288c8905fae773fbdd5381a4509328ee6
percy cordova <cordova_inf@hotmail.com>
2022-03-04 12:21:44 -0500

    configurando tailwinds

--- tailwind.config.js
+++ tailwind.config.js
@@ -59,8 +59,7 @@ const themeColors = {
 
 module.exports = {
   purge: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   darkMode: 'class', // or 'media' or 'class'
   important: true,
