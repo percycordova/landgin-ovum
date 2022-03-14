c453779306a9660aa8c68a06a8c2c2ab1290ea4c
percy cordova <cordova_inf@hotmail.com>
2022-03-14 15:04:56 -0500

    fix(header): se modifico el header

--- src/components/header/menu.js
+++ src/components/header/menu.js
@@ -2,13 +2,10 @@ import Link from 'next/link'
 
 const Menu = () => {
   return (
-    <ul className='flex gap-x-2 text-white text-xl'>
-      <li className='py-4 px-2 cursor-pointer '><Link href="/">Inicio</Link> </li>
-      <li className='py-4 px-2 cursor-pointer '><a href="#acerca">Acerca de Ovum</a> </li>
-      <li className='py-4 px-2 cursor-pointer '><a href="#estructura"> Estructura</a></li>
+    <ul className='flex gap-x-2 text-base font-normal'>
+      <li className='py-4 px-2 cursor-pointer '><Link href="/">Ovum 2022</Link> </li>
       <li className='py-4 px-2 cursor-pointer '><a href="#programa">Programa</a> </li>
       <li className='py-4 px-2 cursor-pointer '><a href="#patrocinadores">Patrocinadores</a> </li>
-      <li className='py-4 px-2 cursor-pointer '><a href="#">Precios</a> </li>
       <li className='py-4 px-2 cursor-pointer '><a href="#registro">Registro</a> </li>
     </ul>
   )
