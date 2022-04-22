6f01a9f7bd18332d7bc0aaf95c6d1f8bb9c9f673
percy cordova <cordova_inf@hotmail.com>
2022-04-22 11:22:42 -0500

    se implemento google analitycs en el proyecto

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,25 @@
-import { ApolloProvider } from '@apollo/client'
-import client from '../apollo/index'
-import '../styles/index.css'
-import 'swiper/css/bundle'
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo/index";
+import Router from "next/router";
+import * as gtag from "../lib/gtag";
+import "../styles/index.css";
+import "swiper/css/bundle";
+import { useEffect } from "react";
 const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
     </ApolloProvider>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
