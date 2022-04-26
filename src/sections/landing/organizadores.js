a7f639527507222f27cb48382c70ea42d1ed55d6
percy cordova <cordova_inf@hotmail.com>
2022-04-26 17:02:22 -0500

    cambiando imagenes

--- src/sections/landing/organizadores.js
+++ src/sections/landing/organizadores.js
@@ -1,17 +1,17 @@
 /* import { useState } from 'react' */
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import {Swiper, SwiperSlide} from 'swiper/react'
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
+import 'swiper/css'
+import 'swiper/css/navigation'
 
 // import required modules
-import { Navigation, Autoplay } from "swiper";
+import {Navigation, Autoplay} from 'swiper'
 
-const Organizadores = ({ idiomas }) => {
-  const { titulo } = idiomas.Organizadores;
-  /* const [width, setWidth] = useState(true)
+const Organizadores = ({idiomas}) => {
+	const {titulo} = idiomas.Organizadores
+	/* const [width, setWidth] = useState(true)
 
   const handleWidth =()=>{
     if(window.innerWidth < 768){
@@ -20,18 +20,24 @@ const Organizadores = ({ idiomas }) => {
       setWidth(true)
     }
   } */
-  return (
-    <section className="relative bg-white flex flex-col justify-center items-center py-8">
-      <div className="w-full md:max-w-256 mt-5 max-w-7xl mx-auto p-0 lg:p-0">
-        <h6 className="text-3xl lg:text-3.5xl text-center font-bold mb-8 text-blue-500">
-         {titulo}
-        </h6>
-        <div className="flex justify-center flex-wrap">
-          <img src="/asociacion.png " className=" lg:w-64 h-32 object-cover" />
-          <img src="/anavih.png " className=" lg:w-60 h-32 object-cover" />
-        </div>
-        {/* version desktop */}
-        {/* <div className="hidden lg:grid grid-cols-4 gap-x-4 gap-y-10">
+	return (
+		<section className='relative bg-white flex flex-col justify-center items-center py-8'>
+			<div className='w-full md:max-w-256 mt-5 max-w-7xl mx-auto p-0 lg:p-0'>
+				<h6 className='text-3xl lg:text-3.5xl text-center font-bold mb-8 text-blue-500'>
+					{titulo}
+				</h6>
+				<div className='flex justify-center flex-wrap items-center'>
+					<img
+						src='/organizadores__logo-asociacion.svg  '
+						className=' lg:w-56 h-30 object-contain'
+					/>
+					<img
+						src='/organizadores__logo-anavih.svg'
+						className=' lg:w-47 h-30 object-contain'
+					/>
+				</div>
+				{/* version desktop */}
+				{/* <div className="hidden lg:grid grid-cols-4 gap-x-4 gap-y-10">
           <div className="">
             <img
               src="/logo-ovum.png"
@@ -89,8 +95,8 @@ const Organizadores = ({ idiomas }) => {
             />
           </div>
         </div> */}
-        {/* slider en mobile */}
-        {/* <div className="lg:hidden">
+				{/* slider en mobile */}
+				{/* <div className="lg:hidden">
           <Swiper
             style={{
               "--swiper-navigation-color": "#C4C4C4",
@@ -173,9 +179,9 @@ const Organizadores = ({ idiomas }) => {
             </SwiperSlide>
           </Swiper>
         </div> */}
-      </div>
-    </section>
-  );
-};
+			</div>
+		</section>
+	)
+}
 
-export default Organizadores;
+export default Organizadores
